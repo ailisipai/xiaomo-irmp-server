a6f995d4399809fa917615a40d480698e56d9647
ailisipai <13585951973@163.com>
2021-10-26 20:43:36 +0800

    xiaomo

---
+++ sh_cloud_server/src/main/java/com/xiaomo/cloud/ShCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaomo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShCloudServerApplication.class, args);
+    }
+
+}
---
+++ sh_cloud_server/src/test/java/com/xiaomo/cloud/ShCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomo.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
